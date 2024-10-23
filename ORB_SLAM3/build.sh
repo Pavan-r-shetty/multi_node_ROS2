db18d2b6ed93ea4d7f133f2a724591b4c6d3403f
Pavan-r-shetty <Pavan-r-shetty>
2024-10-22 18:38:22 -0700

    Add large files with Git LFS

---
+++ ORB_SLAM3/build.sh
@@ -0,0 +1,41 @@
+echo "Configuring and building Thirdparty/DBoW2 ..."
+
+cd Thirdparty/DBoW2
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+cd ../../g2o
+
+echo "Configuring and building Thirdparty/g2o ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+cd ../../Sophus
+
+echo "Configuring and building Thirdparty/Sophus ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+cd ../../../
+
+echo "Uncompress vocabulary ..."
+
+cd Vocabulary
+tar -xf ORBvoc.txt.tar.gz
+cd ..
+
+echo "Configuring and building ORB_SLAM3 ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j3
+echo 'export LD_LIBRARY_PATH=/home/shetty/ros2_ws/src/ORB_SLAM3/lib:/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
